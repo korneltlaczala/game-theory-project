735c0011787e7b3f226e5d6e7e7182917dad1afe
M-SZEWCZAK <125891452+M-SZEWCZAK@users.noreply.github.com>
2025-01-20 12:26:44 +0100

    Fixed allbut periodicity checking

--- solver.py
+++ solver.py
@@ -1,3 +1,8 @@
+import random
+
+from numpy import mean
+
+
 class Game():
 
     def __init__(self):
@@ -71,8 +76,10 @@ class AllButGame(Game):
         max_move = max(self.invalid_moves)
         max_period = n - max_move
         for period_candidate in range(1, max_period + 1):
-            candidate_valid = True
-            for saltus_candidate in range(1,period_candidate):
+            break_flag=False
+            #print(f"period_candidate: {period_candidate}")
+            for saltus_candidate in range(1,period_candidate+1):
+                candidate_valid=True
                 for i in range(n-max_move, n):
                     if self.result[i] != self.result[i - period_candidate]+saltus_candidate:
                         candidate_valid = False
@@ -81,8 +88,23 @@ class AllButGame(Game):
                     self.is_periodic = True
                     self.period_length = period_candidate
                     self.saltus=saltus_candidate
+                    break_flag=True
                     break
+            if break_flag:
+                break
+        if not self.is_periodic:
+            print(f"Either there is no period, or the period is longer than {max_period}")
+            return
 
+        prev_matching = 0
+        for i in range(self.period_length, n):
+            if self.result[i] == self.result[i - self.period_length]+self.saltus:
+                prev_matching += 1
+            else:
+                prev_matching = 0
+            if prev_matching == self.period_length:
+                self.first_period_start = i - 2 * self.period_length + 1
+                break
     def show_result(self):
         print(self.result)
     
@@ -197,7 +219,31 @@ if __name__ == '__main__':
     # print(game)
     # game.update()
 
-    game = AllButGame()
-    game.add_moves([1,2,3,5,8])
-    game.calculate(100)
-    game.show_result()
+    # game = AllButGame()
+    # game.add_moves([2,7])
+    # game.calculate(100)
+    # game.detect_periodicity()
+    # print(game.period_length)
+    # print(game.saltus)
+    # print(game.first_period_start)
+    # game.show_result()
+    # mean_periods=[]
+    # mean_saltuses=[]
+    # periods=[]
+    # saltuses=[]
+    # fes=[]
+    # for j in range(3,100):
+    #     for i in range(50):
+    #         a=random.randint(2,j)
+    #         b=random.randint(2,j)
+    #         while a==b:
+    #             b = random.randint(1,10)
+    #         game=AllButGame()
+    #         game.add_moves([a,b])
+    #         game.calculate(1000)
+    #         game.detect_periodicity()
+    #         periods.append(game.period_length)
+    #         saltuses.append(game.saltus)
+    #         fes.append([a,b])
+    #     mean_periods.append(mean(periods))
+    #     mean_saltuses.append(mean_saltuses)
